cb01be00c4252043778c22c906ec798dfa09b6fe
Murilo Lopes <mloopes11@gmail.com>
2022-02-22 00:36:02 -0300

    Aula 13 - chamando view na pratica

---
+++ laravel/resources/views/clientes.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <title>Curso Laravel</title>
+</head>
+
+<body>
+    <h1>Hello World Clientes <?= $nome ?>
+    </h1>
+</body>
+
+</html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/cliente/{nome}/', function ($nome) {
+    return view('clientes', ['nome' => $nome]);
+});

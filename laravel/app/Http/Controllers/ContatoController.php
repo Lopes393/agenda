3d77778100de3f93ee79d07dfa27cddf026a7982
Murilo Lopes <mloopes11@gmail.com>
2022-02-23 20:29:24 -0300

    config template project

--- laravel/app/Http/Controllers/ContatoController.php
+++ laravel/app/Http/Controllers/ContatoController.php
@@ -13,7 +13,7 @@ class ContatoController extends Controller
      */
     public function index()
     {
-        return view('index');
+        return view('Contato.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class ContatoController extends Controller
      */
     public function create()
     {
-        return view('cadastro');
+        return view('Contato.create');
     }
 
     /**
---
+++ laravel/resources/views/Contato/index.blade.php
@@ -0,0 +1,122 @@
+<section class="caixa">
+    <div class="thead"><i class="ico lista"></i> Lista de contatos</div>
+    <div class="base-lista">
+        <div class="lst">
+            <form action="" method="">
+                <div class="rows">
+                    <div class="col-4">
+                        <select name="txt_id_empresa">
+                            <option selected>Selecione o valor...</option>
+                            <option value="1">Código</option>
+                            <option value="2">Nome</option>
+                            <option value="3">Email</option>
+                            <option value="4">Cidade</option>
+                            <option value="5">Site</option>
+                            <option value="6">Fone</option>
+                        </select>
+                    </div>
+                    <div class="col-6">
+                        <input type="text" name="" placeholder="Valor da pesquisar...">
+                    </div>
+                    <div class="col-2">
+                        <input type="submit" class="btn" value="pesquisar">
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="rows">
+            <div class="col-12">
+                <span class="itens"><i class="ico-list"></i> <b>18 </b>clientes cadastrados</span>
+            </div>
+        </div>
+        <div class="tabela-responsiva">
+            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                <thead>
+                    <tr>
+                        <th align="left">Nome</th>
+                        <th align="left">Email</th>
+                        <th align="center">Telefone</th>
+                        <th align="center">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Manoel Jailton Nascimento</td>
+                        <td>mjailton@gmail.com</td>
+                        <td align="center">9899924667</td>
+                        <td align="center">
+                            <a href="formulario.html" class="btn alterar">Editar</a>
+                            <a href="formulario.html" class="btn excluir">Excluir</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Manoel Jailton Nascimento</td>
+                        <td>mjailton@gmail.com</td>
+                        <td align="center">9899924667</td>
+                        <td align="center">
+                            <a href="formulario.html" class="btn alterar">Editar</a>
+                            <a href="formulario.html" class="btn excluir">Excluir</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Manoel Jailton Nascimento</td>
+                        <td>mjailton@gmail.com</td>
+                        <td align="center">9899924667</td>
+                        <td align="center">
+                            <a href="formulario.html" class="btn alterar">Editar</a>
+                            <a href="formulario.html" class="btn excluir">Excluir</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Manoel Jailton Nascimento</td>
+                        <td>mjailton@gmail.com</td>
+                        <td align="center">9899924667</td>
+                        <td align="center">
+                            <a href="formulario.html" class="btn alterar">Editar</a>
+                            <a href="formulario.html" class="btn excluir">Excluir</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Manoel Jailton Nascimento</td>
+                        <td>mjailton@gmail.com</td>
+                        <td align="center">9899924667</td>
+                        <td align="center">
+                            <a href="formulario.html" class="btn alterar">Editar</a>
+                            <a href="formulario.html" class="btn excluir">Excluir</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Manoel Jailton Nascimento</td>
+                        <td>mjailton@gmail.com</td>
+                        <td align="center">9899924667</td>
+                        <td align="center">
+                            <a href="formulario.html" class="btn alterar">Editar</a>
+                            <a href="formulario.html" class="btn excluir">Excluir</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Manoel Jailton Nascimento</td>
+                        <td>mjailton@gmail.com</td>
+                        <td align="center">9899924667</td>
+                        <td align="center">
+                            <a href="formulario.html" class="btn alterar">Editar</a>
+                            <a href="formulario.html" class="btn excluir">Excluir</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Manoel Jailton Nascimento</td>
+                        <td>mjailton@gmail.com</td>
+                        <td align="center">9899924667</td>
+                        <td align="center">
+                            <a href="formulario.html" class="btn alterar">Editar</a>
+                            <a href="formulario.html" class="btn excluir">Excluir</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <p class="nao-encontrado">Nenhum registro encontrado</p>
+
+    </div>
+    </div>
+</section>

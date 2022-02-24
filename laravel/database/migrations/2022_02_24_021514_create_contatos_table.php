93d02c6a5db4f24f7cf98216d62f3c09fa5b8178
Murilo Lopes <mloopes11@gmail.com>
2022-02-23 23:26:21 -0300

    create migration contacts

--- laravel/database/migrations/2022_02_24_021514_create_contatos_table.php
+++ laravel/database/migrations/2022_02_24_021514_create_contatos_table.php
@@ -4,25 +4,36 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
         Schema::create('contatos', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('endereco');
+            $table->string('numero');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('cep');
+            $table->string('uf');
+            $table->string('celular');
+            $table->string('cpf');
+            $table->string('sexo')->nullable();
+            $table->string('email');
+            $table->date('data_nascimento');
+            $table->string('observacao');
+            $table->string('senha');
+            $table->string('rg');
+            $table->date('data_cadastro');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
